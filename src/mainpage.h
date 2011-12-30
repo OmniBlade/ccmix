a02b558c85381279c81085769f481c976889caba
Ivosh <ivosh-l@lm-ivosh.(none)>
2011-12-30 15:00:22 +0100

    updated documentation

---
+++ src/mainpage.h
@@ -0,0 +1,29 @@
+/*! \mainpage TSunmix
+ *
+ * \section About TSunmix
+ *
+ * TSunmix is tool for extraction files from Tiberian sun's mix archive. 
+ * 
+ * \section Licence
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the <a href="http://www.gnu.org/licenses/gpl.html">GNU General Public License</a> as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the <a href="http://www.gnu.org/licenses/gpl.html">GNU General Public License</a> for more details.
+
+You should have received a copy of the <a href="http://www.gnu.org/licenses/gpl.html">GNU General Public License</a> along with this program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * \section Credits
+ * 
+ * TSunmix is based on <a href="http://xhp.xwis.net/documents/MIX_Format.html">mix file format specification</a> by Olaf van der Spek. It's inspired by XCC Utilities. 
+ * For list of known file names XCC's global mix database.dat is used.
+ * 
+ * Thanks to Olaf van der Spek for support and sharing his "mix file knowledge".
+ *
+ *
+ * \sa
+ * TS mix file format specification (http://xhp.xwis.net/documents/MIX_Format.html)
+ * \sa
+ * XCC homepage (http://xhp.xwis.net/)
+ * \sa
+ * XCC google code page (http://code.google.com/p/xcc/)
+ */
